0d094e17982aaa963b8620b9930a622cadde8640
Nasir Iqbal <60297068+nasir733@users.noreply.github.com>
2021-04-16 06:00:21 +0500

    prefecthing assets

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 
+const cacheImages = (images) =>
+  images.map((image) => {
+    if (typeof image == "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync;
+    }
+  });
 export default function App() {
-  return <AppLoading />;
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = async () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1536148935331-408321065b18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=633&q=80",
+    ]);
+  };
+  const onFinish = () => setIsReady(true);
+  return isReady ? (
+    <Text>ready</Text>
+  ) : (
+    <AppLoading
+      startAsync={loadAssets}
+      onError={console.error}
+      onFinish={onFinish}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
